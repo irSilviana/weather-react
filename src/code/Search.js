e4c0223154c758caa588a6254881b4abacf8784c
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-03-02 23:07:57 +0700

    Update Search.js

--- src/code/Search.js
+++ src/code/Search.js
@@ -1,38 +1,41 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Weather from "./Weather";
+import React from "react";
+// import axios from "axios";
+// import Weather from "./Weather";
 import "./Search.css";
 
 export default function Search() {
-  let [city, setCity] = useState(null);
-  let [weatherData, setWeatherData] = useState(null);
+  // let [city, setCity] = useState(null);
+  // let [weatherData, setWeatherData] = useState(null);
 
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
+  // function changeCity(event) {
+  //   setCity(event.target.value);
+  // }
 
-  function showTemperature(response) {
-    let temperature = response.data.main.temp;
-    let description = response.data.weather[0].main;
-    let humidity = response.data.main.humidity;
-    let wind = response.data.wind.speed;
-    let icon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  // function showTemperature(response) {
+  //   let temperature = response.data.main.temp;
+  //   let description = response.data.weather[0].main;
+  //   let humidity = response.data.main.humidity;
+  //   let wind = response.data.wind.speed;
+  //   let icon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 
-    setWeatherData({ temperature, description, humidity, wind, icon });
-  }
+  //   setWeatherData({ temperature, description, humidity, wind, icon });
+  // }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  // function handleSubmit(event) {
+  //   event.preventDefault();
 
-    let apiKey = `125089b53f00feddd6fbd602dc6cec7a`;
-    let unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  //   let apiKey = `125089b53f00feddd6fbd602dc6cec7a`;
+  //   let unit = "metric";
+  //   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
-    axios.get(apiUrl).then(showTemperature);
-  }
+  //   axios.get(apiUrl).then(showTemperature);
+  // }
 
   let form = (
-    <form id="search-form" onSubmit={handleSubmit}>
+    <form
+      id="search-form"
+      // onSubmit={handleSubmit}
+    >
       <input
         type="search"
         id="input-city"
@@ -40,7 +43,7 @@ export default function Search() {
         placeholder="Enter a city"
         autoFocus="on"
         autoComplete="off"
-        onChange={changeCity}
+        // onChange={changeCity}
       />
       <input
         type="submit"
