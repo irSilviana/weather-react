9ed92b6f978772fa165b1ced726caea486d603e1
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-03-02 22:52:15 +0700

    Update App.js & App.css

--- src/code/App.js
+++ src/code/App.js
@@ -1,15 +1,45 @@
 import "./App.css";
+import Cities from "./Cities";
 import Search from "./Search";
-import Weather from "./Weather";
+import CityTime from "./CityTime";
+import Temperature from "./Temperature";
+import Forecast from "./Forecast";
+import Footer from "./Footer";
 
-function App() {
+// let background = {
+//   backgroundPosition: "center",
+//   backgroundRepeat: "no-repeat",
+//   backgroundImage: `url(${require("/images/background/10n@2x.png")} )`,
+// };
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <Search />
-      <Weather />
+    <div className="App" /* style={background} */>
+      <div className="container">
+        <div className="weather-page">
+          <div className="weather-app">
+            {/* List of Cities */}
+            <Cities />
+            {/* Search Form */}
+            <Search />
+            {/* Main Body */}
+            <div className="row">
+              {/* Left Side */}
+              <div className="col">
+                <CityTime />
+              </div>
+              {/* Right Side */}
+              <div className="col">
+                <Temperature />
+              </div>
+            </div>
+            {/* Forecast */}
+            <Forecast />
+          </div>
+          {/* Footer */}
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
