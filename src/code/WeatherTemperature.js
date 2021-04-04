8ad8341c7c8adaf80a23ef85ce94d2711b12c7ab
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-05 03:33:14 +0700

    Update WeatherTemperature.js

--- src/code/WeatherTemperature.js
+++ src/code/WeatherTemperature.js
@@ -2,43 +2,71 @@ import React, { useState } from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
+  let [unit, setUnit] = useState(props.unit);
+  const weather = [{ unit: props.unit, temp: props.temperature }];
+  let temperature = null;
 
-  function showFahrenheit(event) {
+  function updateWeather() {
+    if (props.unit === "metric") {
+      temperature = (props.temperature * 9) / 5 + 32;
+      weather.push({ unit: "imperial", temp: temperature });
+    } else if (props.unit === "imperial") {
+      temperature = ((props.temperature - 32) * 5) / 9;
+      weather.push({ unit: "metric", temp: temperature });
+    }
+  }
+  updateWeather();
+
+  function result() {
+    let result = [];
+    if (unit === "imperial") {
+      for (let i = 0; i < weather.length; i++) {
+        if (weather[i].unit === "imperial") {
+          result.push(weather[i].temp);
+        }
+      }
+      return result;
+    } else if (unit === "metric") {
+      for (let i = 0; i < weather.length; i++) {
+        if (weather[i].unit === "metric") {
+          result.push(weather[i].temp);
+        }
+      }
+      return result;
+    }
+  }
+
+  function setFahrenheit(event) {
     event.preventDefault();
-    setUnit("fahrenheit");
+    setUnit("imperial");
   }
 
-  function showCelsius(event) {
+  function setCelsius(event) {
     event.preventDefault();
-    setUnit("celsius");
+    setUnit("metric");
   }
 
-  function fahrenheit() {
-    return (props.celcius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
+  if (unit === "metric") {
     return (
       <div className="WeatherTemperature">
-        <span className="Temperature">{Math.round(props.celcius)}</span>
+        <span className="Temperature">{Math.round(result())}</span>
         <span className="units">
           <button href="#" className="active">
             °C
           </button>
           |
-          <button href="#" onClick={showFahrenheit}>
+          <button href="#" onClick={setFahrenheit}>
             °F
           </button>
         </span>
       </div>
     );
-  } else {
+  } else if (unit === "imperial") {
     return (
       <div className="WeatherTemperature">
-        <span className="Temperature">{Math.round(fahrenheit())}</span>
+        <span className="Temperature">{Math.round(result())}</span>
         <span className="units">
-          <button href="#" onClick={showCelsius}>
+          <button href="#" onClick={setCelsius}>
             °C
           </button>{" "}
           |
